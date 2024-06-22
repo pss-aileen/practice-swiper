f4ad389ae76c1e8878d109bd83e70232ad388e25
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-06-22 13:41:02 +0900

    Wip: Implement pc version
    Next is responsive

--- team/js/main.js
+++ team/js/main.js
@@ -1,17 +1,31 @@
 const swiper = new Swiper('.swiper', {
-  direction: 'vertical',
-  loop: true,
+  // loop: true,
+  grabCursor: true,
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
+  slidesPerView: 1.2,
+  spaceBetween: 24,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '#my-swiper-button-next',
+    prevEl: '#my-swiper-button-prev',
   },
 
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1.8,
+      spaceBetween: 24
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2.4,
+      spaceBetween: 48
+    },
+    1024: {
+      slidesPerView: 2.8,
+      spaceBetween: 48,
+    },
+    1280: {
+      slidesPerView: 3.5,
+      spaceBetween: 48,
+    }
+  }
 });
